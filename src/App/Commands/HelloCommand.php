8f9b990c1123c977d448aca3760a13c14dd58f76
Alexandra <69279765+AlexandraVashkel@users.noreply.github.com>
2021-06-26 02:21:37 +0300

    Create HelloCommand.php

---
+++ src/App/Commands/HelloCommand.php
@@ -0,0 +1,24 @@
+<?php
+namespace Console\App\Commands;
+
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Input\InputArgument;
+
+class HelloCommand extends Command
+{
+    protected function configure()
+    {
+        $this->setName('hello')
+            ->setDescription('Prints Hello')
+            ->setHelp('Demonstration of custom commands created by Symfony Console component.')
+            ->addArgument('username', InputArgument::REQUIRED, 'Pass the username.');
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $output->writeln(sprintf('Hello, %s!', $input->getArgument('username')));
+        return exit(0);
+    }
+}
